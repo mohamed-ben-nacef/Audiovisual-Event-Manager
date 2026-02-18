e6ee0f041596609d3d9deac0c3af926a00cc110f
mohamed ben nacef <mohamedbennacefdevlab@gmail.com>
2026-02-18 22:25:11 +0100

    url

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosInstance } from "axios"
 import { AuthTokens, User } from "@/types"
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api"
+const API_URL = "https://events-se67.onrender.com/api"
 
 class ApiClient {
   private client: AxiosInstance
